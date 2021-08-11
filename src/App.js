e98e1ba6e38ad0cdf32cf5ba48fa9d57747ce7e3
EzekielUmanmah <umanmah234@gmail.com>
2021-08-11 08:24:02 -0400

    install self-hosted fonts

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 function App() {
   return (
     <Router>
+      <div style={{ fontFamily: 'montserrat' }}>This is a test of the fonts</div>
+      <div style={{ fontFamily: 'bitter' }}>This is a test of the fonts</div>
       <Switch>
         <Route path="/search">Search</Route>
         <Route path="/">Home</Route>
--- src/styles/globals.js
+++ src/styles/globals.js
@@ -3,10 +3,11 @@ import { normalize } from 'styled-normalize';
 
 const GlobalStyles = createGlobalStyle`
     ${normalize};
-    
+
     body {
         background: teal;
         font-size: 1.6rem;
+        font-family: montserrat;
     }
     a {
         text-decoration: none;
