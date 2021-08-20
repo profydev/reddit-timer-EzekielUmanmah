5ffef1a263fd0c983c6ae79e79b730204f408647
EzekielUmanmah <umanmah234@gmail.com>
2021-08-20 04:10:46 -0400

    remove integration tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,7 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
-import userEvent from '@testing-library/user-event';
+import { render } from '@testing-library/react';
 import App from './App';
 
-const setup = () => {
-  render(<MemoryRouter><App /></MemoryRouter>);
-};
-
-describe('Header Links', () => {
-  const table = [['AbouT', 'About'], ['how it works', 'How it works'], ['redditLogo.svg', 'Home']];
-
-  test.each(table)('"%s" link points to the "%s" page', (linkName, heading) => {
-    setup();
-    const link = screen.getByRole('link', { name: new RegExp(linkName, 'i') });
-    userEvent.click(link);
-    expect(screen.getByRole('heading', { name: new RegExp(heading, 'i') })).toBeInTheDocument();
-  });
+test('renders App', () => {
+  render(<App />);
 });
