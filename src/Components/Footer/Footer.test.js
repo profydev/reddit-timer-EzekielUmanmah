4b8f5934aa61026359fa12b10caf16cf84494a4e
EzekielUmanmah <umanmah234@gmail.com>
2021-08-24 18:51:43 -0400

    fix header/footer integration tests

--- src/Components/Footer/Footer.test.js
+++ src/Components/Footer/Footer.test.js
@@ -38,7 +38,7 @@ describe('Footer links', () => {
     const logo = screen.getByRole('link', { name: /^logo\.svg/i });
     userEvent.click(logo);
 
-    expect(screen.getByRole('heading', { name: /home/i })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: /no reactions/i })).toBeInTheDocument();
   });
 
   test('Terms & Privacy points to /terms', () => {
